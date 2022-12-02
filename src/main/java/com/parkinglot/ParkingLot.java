bff5fdba6779e24b3c57d0958ca82bfb01d580d1
WONGCH9 <wongch9@oocl.com>
2022-12-02 14:10:31 +0800

    feat: added feature & test should_return_nothing_when_park_car_given_a_parking_lot_without_any_position_and_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -6,8 +6,15 @@ import java.util.Map;
 public class ParkingLot {
 
     private Map<ParkingTicket, Car> parkedPosition = new HashMap<>();
+    private int capacity;
+
+    public ParkingLot(int capacity){
+        this.capacity = capacity;
+    }
 
     public ParkingTicket park(Car car){
+        if(parkedPosition.size() >= capacity)
+            return null;
         ParkingTicket ticket = new ParkingTicket();
         parkedPosition.put(ticket,car);
         return ticket;
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -11,7 +11,7 @@ public class ParkingLotTest {
     @Test
     void should_return_ticket_when_park_given_a_car(){
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         Car car = new Car();
 
         //when
@@ -24,7 +24,7 @@ public class ParkingLotTest {
     @Test
     void should_return_car_when_fetch_given_a_ticket(){
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         Car car = new Car();
         ParkingTicket ticket = parkingLot.park(car);
 
@@ -38,7 +38,7 @@ public class ParkingLotTest {
     @Test
     void should_return_right_car_when_fetch_twice_given_two_parked_cars_and_tickets(){
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         Car aliceCar = new Car();
         Car bobCar = new Car();
         ParkingTicket aliceTicket = parkingLot.park(aliceCar);
@@ -56,7 +56,7 @@ public class ParkingLotTest {
     @Test
     void should_return_nothing_when_fetch_car_given_a_parking_lot_and_wrong_ticket(){
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         Car bobCar = new Car();
 
         //when
@@ -69,7 +69,7 @@ public class ParkingLotTest {
     @Test
     void should_return_nothing_when_fetch_car_given_a_parking_lot_and_used_ticket(){
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         Car bobCar = new Car();
         ParkingTicket bobTicket = parkingLot.park(bobCar);
         Car bob = parkingLot.fetch(bobTicket) ;
@@ -81,6 +81,21 @@ public class ParkingLotTest {
         assertEquals(null, aliceFetchedCar);
     }
 
+    @Test
+    void should_return_nothing_when_park_car_given_a_parking_lot_without_any_position_and_car(){
+        //given
+        ParkingLot parkingLot = new ParkingLot(1);
+
+        //when
+        Car bob = new Car();
+        ParkingTicket bobTickete = parkingLot.park(bob);
+        Car aliceCar = new Car();
+        ParkingTicket aliceTicket = parkingLot.park(aliceCar);
+        aliceCar =  parkingLot.fetch(aliceTicket) ;
+
+        //then
+        assertEquals(null, aliceCar);
+    }
 
 
 }
