e921ec6bf2ebecb022483e677e75e2c946e2133d
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-02 16:15:36 +0800

    feat: should_return_parked_car_when_fetch_given_parked_parking_lot_and_car

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -1,5 +1,6 @@
 package com.parkinglot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,7 @@ public class StandardParkingBoyTest {
     // Given a parking lot, a parking boy, and a car, when park the car, then return
     // a parking ticket.
     @Test
-    public void should_return_ticket_when_park_given_parking_boy_and_car() {
+    public void should_return_ticket_when_park_given_parking_lot_and_car() {
         // given
         StandardParkingBoy parkingBoy = new StandardParkingBoy(new ParkingLot());
         Car car = new Car();
@@ -21,6 +22,17 @@ public class StandardParkingBoyTest {
 
     // Given a parking lot with a parked car, a parking boy, and a ticket, when
     // fetch the car, them return the parked car.
+    @Test
+    public void should_return_parked_car_when_fetch_given_parked_parking_lot_and_car() {
+        // given
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(new ParkingLot());
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        // when
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        // then
+        assertEquals(car, fetchedCar);
+    }
 
     // Given a parking lot, a parking boy, two cars, and two tickets, when fetch the
     // car twice, them return corresponding cars.
