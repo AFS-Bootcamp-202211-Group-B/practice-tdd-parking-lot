7e5a842e55d2bffbde8855976d2fdb146c3bb848
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-02 16:13:03 +0800

    feat: should_return_ticket_when_park_given_parking_boy_and_car

---
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -0,0 +1,13 @@
+package com.parkinglot;
+
+public class StandardParkingBoy {
+    private ParkingLot parkingLot;
+
+    public StandardParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public Ticket park(Car car) {
+        return parkingLot.park(car);
+    }
+}
---
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -0,0 +1,37 @@
+package com.parkinglot;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class StandardParkingBoyTest {
+    // Given a parking lot, a parking boy, and a car, when park the car, then return
+    // a parking ticket.
+    @Test
+    public void should_return_ticket_when_park_given_parking_boy_and_car() {
+        // given
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(new ParkingLot());
+        Car car = new Car();
+        // when
+        Ticket ticket = parkingBoy.park(car);
+        // then
+        assertNotNull(ticket);
+
+    }
+
+    // Given a parking lot with a parked car, a parking boy, and a ticket, when
+    // fetch the car, them return the parked car.
+
+    // Given a parking lot, a parking boy, two cars, and two tickets, when fetch the
+    // car twice, them return corresponding cars.
+
+    // Given a parking lot, a parking boy, and a wrong ticket, when fetch the car,
+    // then return null, and throw "Unrecognized parking ticket".
+
+    // Given a parking lot, a parking boy, and a used ticket, when fetch the car,
+    // then return null, and throw "Unrecognized parking ticket".
+
+    // Given a full parking lot, a parking boy, and a car, when park the car, then
+    // return null, and throw "No available position".
+
+}
