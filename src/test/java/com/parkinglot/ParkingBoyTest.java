1059ca4edb305af110adf00b80d0b25d6a3bcecf
tokaho <tokaho2000@gmail.com>
2022-12-02 18:17:01 +0800

    [Test] should_return_two_right_car_from_corresponding_ticket_when_fetch_given_2parkingLots_both_with_parked_car_and_two_ticket

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -135,4 +135,25 @@ public class ParkingBoyTest {
         assertFalse(parkingLot1.has(ticket));
         assertTrue(parkingLot2.has(ticket));
     }
+
+//    Given a standard parking boy, who manage two parking lots, both with a parked car, and two parking ticket, When
+//    fetch the car twice, Then return the right car with each ticket
+    @Test
+    void should_return_two_right_car_from_corresponding_ticket_when_fetch_given_2parkingLots_both_with_parked_car_and_two_ticket(){
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingLot1.park(car1);
+        Ticket ticket2 = parkingLot2.park(car2);
+        ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
+        //when
+        Car returnCar1 = parkingBoy.fetch(ticket1);
+        Car returnCar2 = parkingBoy.fetch(ticket2);
+
+        //then
+        assertEquals(car1,returnCar1);
+        assertEquals(car2,returnCar2);
+    }
 }
