5268589f236bd064f8f17b67e8a7a8b07dc0f941
Antony Choi <antony.choi@oocl.com>
2022-12-02 14:16:53 +0800

    refactor

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ParkingLotTest {
+class ParkingLotTest {
     @Test
     void should_return_ticket_when_park_given_a_parkingLot() {
         //given
