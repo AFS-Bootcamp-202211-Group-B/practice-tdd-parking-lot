75de1f40d47114b0cf18637ab4095ace5942a115
tokaho <tokaho2000@gmail.com>
2022-12-02 16:13:11 +0800

    [Feat]should_return_ticket_when_park_given_parkingLot_parkingBoy_car

---
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -0,0 +1,20 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingBoyTest {
+    @Test
+    void should_return_ticket_when_park_given_parkingLot_parkingBoy_car(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingBoy.park(car);
+
+        //then
+        assertNotNull(ticket);
+    }
+}
