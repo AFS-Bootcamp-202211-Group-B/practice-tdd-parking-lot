add6ee9fba640526dce803ab9305905440440fa3
leungpo <polly.leung@oocl.com>
2022-12-02 13:46:08 +0800

    feat:[Story1] add test case forshould_return_parked_car_when_fetch_given_a_parked_car and implement return parked car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,7 +1,13 @@
 package com.parkinglot;
 
 public class ParkingLot {
+    private Car parkedCar;
     public Ticket park(Car car) {
+        this.parkedCar = car;
         return new Ticket();
+    }
+
+    public Car fetch(Ticket ticket) {
+        return this.parkedCar;
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -14,5 +15,17 @@ public class ParkingLotTest {
         Ticket ticket = parkingLot.park(car);
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_parked_car_when_fetch_given_a_parked_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+        //when
+        Car fetchedCar = parkingLot.fetch(ticket);
+        //then
+        assertEquals(car, fetchedCar);
     }
 }
