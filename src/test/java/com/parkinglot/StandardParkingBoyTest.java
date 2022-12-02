4e070b6cbc4d2a654fa430702825685b5c3eb58d
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-02 16:16:46 +0800

    test: should_return_corresponding_cars_when_fetch_twice_given_parking_lot_and_two_cars

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -23,7 +23,7 @@ public class StandardParkingBoyTest {
     // Given a parking lot with a parked car, a parking boy, and a ticket, when
     // fetch the car, them return the parked car.
     @Test
-    public void should_return_parked_car_when_fetch_given_parked_parking_lot_and_car() {
+    public void should_return_parked_car_when_fetch_given_parking_lot_and_parked_car() {
         // given
         StandardParkingBoy parkingBoy = new StandardParkingBoy(new ParkingLot());
         Car car = new Car();
@@ -36,6 +36,22 @@ public class StandardParkingBoyTest {
 
     // Given a parking lot, a parking boy, two cars, and two tickets, when fetch the
     // car twice, them return corresponding cars.
+    @Test
+    public void should_return_corresponding_cars_when_fetch_twice_given_parking_lot_and_two_cars() {
+        // given
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(new ParkingLot());
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        // when
+        Car fetchedCar1 = parkingBoy.fetch(ticket1);
+        Car fetchedCar2 = parkingBoy.fetch(ticket2);
+        // then
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+
+    }
 
     // Given a parking lot, a parking boy, and a wrong ticket, when fetch the car,
     // then return null, and throw "Unrecognized parking ticket".
