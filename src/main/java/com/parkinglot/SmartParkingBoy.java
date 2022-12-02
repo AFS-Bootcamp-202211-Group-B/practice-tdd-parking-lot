de18f39054af73af2947a224ffd4dafbdc2c99a5
tokaho <tokaho2000@gmail.com>
2022-12-02 18:58:47 +0800

    [refactor] remove unused constructor

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -4,9 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy{
-    public SmartParkingBoy(ParkingLot parkingLot) {
-        super(parkingLot);
-    }
+
 
     public SmartParkingBoy(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
