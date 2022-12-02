5f34df80b41c5ec713a3319ba35dacb4043ce42b
tokaho <tokaho2000@gmail.com>
2022-12-02 16:20:20 +0800

    [Test]should_return_right_Car_when_Fetch_given_parkingLot_two_parked_cars_and_tickets

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -10,4 +10,8 @@ public class ParkingBoy {
     public Ticket park(Car car) {
         return this.parkingLot.park(car);
     }
+
+    public Car fetch(Ticket ticket) {
+        return this.parkingLot.fetch(ticket);
+    }
 }
--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
@@ -16,5 +17,36 @@ public class ParkingBoyTest {
 
         //then
         assertNotNull(ticket);
+    }
+
+
+    @Test
+    void should_return_Car_when_Fetch_given_parkingLot_Ticket(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        //when
+        Car returnCar = parkingBoy.fetch(ticket);
+        //then
+        assertEquals(car,returnCar);
+    }
+
+    @Test
+    void should_return_right_Car_when_Fetch_given_parkingLot_two_parked_cars_and_tickets(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        //when
+        Car returnCar1 = parkingBoy.fetch(ticket1);
+        Car returnCar2 = parkingBoy.fetch(ticket2);
+        //then
+        assertEquals(car1,returnCar1);
+        assertEquals(car2,returnCar2);
     }
 }
