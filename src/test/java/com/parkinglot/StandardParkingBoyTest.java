a0b64fa290aee43575bd8fa8ff8a29b00cbe443b
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-02 16:19:33 +0800

    test: should_return_unrecognized_parking_ticket_exception_when_fetch_given_used_ticket

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -67,8 +67,22 @@ public class StandardParkingBoyTest {
         assertEquals("Unrecognized parking ticket.", exception.getMessage());
 
     }
+
     // Given a parking lot, a parking boy, and a used ticket, when fetch the car,
     // then return null, and throw "Unrecognized parking ticket".
+    @Test
+    public void should_return_unrecognized_parking_ticket_exception_when_fetch_given_used_ticket() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+        parkingLot.fetch(ticket);
+        // when
+        // then
+        Exception exception = assertThrows(UnrecognizedTicketException.class, () -> parkingLot.fetch(ticket));
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+
+    }
 
     // Given a full parking lot, a parking boy, and a car, when park the car, then
     // return null, and throw "No available position".
