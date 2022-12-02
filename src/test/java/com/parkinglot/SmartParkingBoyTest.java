8cca9bba503bc942532d4367189fed6b6f8ba909
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-02 22:15:26 +0800

    [story 5] feat: should_return_ticket_when_park_given_parking_lot_and_car

---
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -0,0 +1,49 @@
+package com.parkinglot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class SmartParkingBoyTest {
+    // Story 5
+    // Given two empty parking lots, a smart parking boy, and a car, when park the
+    // car, then return a parking ticket.
+    @Test
+    public void should_return_ticket_when_park_given_parking_lot_and_car() {
+        // given
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(new ParkingLot());
+        Car car = new Car();
+        // when
+        Ticket ticket = parkingBoy.park(car);
+        // then
+        assertNotNull(ticket);
+
+    }
+    // Given first full parking lot and second parking lot with available positions,
+    // a smart parking boy, and a car, when park the car, then return a parking
+    // ticket from second parking lot.
+
+    // Given first parking lot with 1 empty position, capacity 5, and second parking
+    // lot with 2 empty positions, capacity 3, a smart parking boy, and a car, when
+    // park the car, then return a parking ticket from second parking lot.
+
+    // Given two parking lots with a parked car each, a smart parking boy, two cars,
+    // and two tickets, when fetch the car twice, then return corresponding cars.
+
+    // Given two parking lots, a smart parking boy, and an unrecognized ticket in
+    // any parking lots, when fetch the car, then throw "Unrecognized parking
+    // ticket".
+
+    // Given two parking lots, a smart parking boy, and a used ticket in any one of
+    // the parking lots, when fetch the car, then and throw "Unrecognized parking
+    // ticket".
+
+    // Given two full parking lots, a smart parking boy, and a car, when park the
+    // car, then throw "No available position".
+}
