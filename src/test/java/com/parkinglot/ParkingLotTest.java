b8a04ccd2f55a2c4e91d134ea1d77e9d26496c5f
VINCENT TAM (SYS-TSV-ISD-OOCLL/HKG) <TAMVI@OOCL.COM>
2022-12-02 14:02:11 +0800

    [Story]Feat: should_return_the_right_car_with_each_ticket_when_get_car_twice_given_a_parking_with_two_parked_cars

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -34,4 +34,25 @@ public class ParkingLotTest {
         assertEquals(car , parkedCar);
     }
 
+    @Test
+    void should_return_the_right_car_with_each_ticket_when_get_car_twice_given_a_parking_with_two_parked_cars(){
+        ///Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Car car2 = new Car();
+
+        Ticket ticket = ParkingLot.park(car);
+        Ticket ticket2= ParkingLot.park(car2);
+
+        //When
+        Car parkedCar = parkingLot.getCar(ticket);
+        Car parkedCar2 = parkingLot.getCar(ticket2);
+
+        //Then
+        assertEquals(car , parkedCar);
+        assertEquals(car2 , parkedCar2);
+    }
+
+
+
 }
