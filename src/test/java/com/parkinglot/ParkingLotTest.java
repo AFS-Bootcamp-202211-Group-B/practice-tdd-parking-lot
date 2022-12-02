197b84c20cc3fe4674d32081238cbc0c161f0ae1
Joyce Li <joyceli214@yahoo.com.hk>
2022-12-02 11:44:49 +0800

    feat: should_return_ticket_when_park_given_car

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,34 @@
+package com.parkinglot;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ParkingLotTest {
+    // AC1
+    // Given a parking lot, and a car, when park the car, then return a parking
+    // ticket.
+    @Test
+    public void should_return_ticket_when_park_given_car() {
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park();
+        assertNotNull(ticket);
+
+    }
+    // Given a parking lot with a parked car, and a ticket, when fetch the car, then
+    // return the parked car.
+
+    // AC2
+    // Given a parking lot, two cars, and two tickets, when fetch the car twice,
+    // them return corresponding cars.
+
+    // AC3
+    // Given a parking lot, and a wrong ticket, when fetch the car, then return
+    // null.
+    // Given a parking lot, and no ticket, when fetch the car, then return null.
+    // AC4
+    // Given a parking lot, and a used ticket, when fetch the car, then return null.
+    // AC5
+    // Given a full parking lot, and a car, when park the car, then return null.
+}
