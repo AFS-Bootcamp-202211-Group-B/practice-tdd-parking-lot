bd1f3d8f47ec4e9a402e1ecfcadb409ec18a04cf
WONGCH9 <wongch9@oocl.com>
2022-12-03 22:50:00 +0800

    [story4] feat: added feature and test should_return_ticket_when_parking_boy_park_given_one_full_and_one_available_parking_lot_and_a_car

--- src/test/java/com/parkinglot/MultipleParkingLotsTest.java
+++ src/test/java/com/parkinglot/MultipleParkingLotsTest.java
@@ -22,5 +22,24 @@ public class MultipleParkingLotsTest {
         assertNotNull(ticket);
     }
 
+    @Test
+    void should_return_ticket_when_parking_boy_park_given_one_full_and_one_available_parking_lot_and_a_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(parkingLot1);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        parkingBoy.addParkingLot(parkingLot2);
+        Car car = new Car();
+        parkingLot1.park(car);
+        Car car2 = new Car();
+
+        //when
+        ParkingTicket ticket = parkingBoy.park(car2);
+
+        //then
+        assertNotNull(ticket);
+    }
+
+
 
 }
