6b650b4f0eb21492504ae2bac04a9547727a2cbc
VINCENT TAM (SYS-TSV-ISD-OOCLL/HKG) <TAMVI@OOCL.COM>
2022-12-03 22:16:48 +0800

    [Story4]Feat : Two Parking Lot Test: should_return_park_car_ticket_when_only_second_parking_lots_available_first_parking_lots_is_full

--- src/test/java/com/parkinglot/ParkingLotManagerTest.java
+++ src/test/java/com/parkinglot/ParkingLotManagerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParkingLotManagerTest {
 
     @Test
-    void should_park_to_first_parking_lot_when_park_car_given_standard_manager_manages_two_parking_lots_both_available() {
+    void should_return_park_ticket_to_first_parking_lot_when_two_parking_lots_both_available() {
         // given
         ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
@@ -30,6 +30,48 @@ class ParkingLotManagerTest {
         assertEquals(9, firstParkingLot.getAvailablePositionCount());
         assertEquals(10, secondParkingLot.getAvailablePositionCount());
     }
+    @Test
+    void should_return_park_car_ticket_when_only_second_parking_lots_available_first_parking_lots_is_full() {
+        // given
+        ParkingLot firstParkingLot = new ParkingLot(0);
+        ParkingLot secondParkingLot = new ParkingLot();
+
+        List<ParkingLot> parkingLots = new ArrayList<>();
+
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLots);
+
+        // when
+        Ticket ticket = parkingLotManager.park(new Car());
+
+        // then
+        assertNotNull(ticket);
+        assertEquals(0, firstParkingLot.getAvailablePositionCount());
+        assertEquals(9, secondParkingLot.getAvailablePositionCount());
+    }
+    @Test
+    void should_return_the_right_car_with_each_ticket_when_get_car_twice_two_lots_both_with_parked_cars_and_two_parking_tickets() {
+
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+
+        List<ParkingLot> parkingLots = new ArrayList<>();
 
 
+
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+
+        Ticket firstTicket = firstParkingLot.park(firstCar);
+        Ticket secondTicket = secondParkingLot.park(secondCar);
+
+
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+
+        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLots);
+
+
+    }
 }
