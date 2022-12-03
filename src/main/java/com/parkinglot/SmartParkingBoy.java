585354b13fbdd6f599369cae9a68acc6505ef205
leungpo <polly.leung@oocl.com>
2022-12-03 11:00:15 +0800

    feat:[Story5] implement should_park_to_second_parking_lot_when_park_given_2_parking_lots_first_one_has_less_available_postiion_than_second_one

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -1,10 +1,28 @@
 package com.parkinglot;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class SmartParkingBoy extends StandardParkingBoy{
+public class SmartParkingBoy{
 
+    private final List<ParkingLot> parkingLots;
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
-        super(parkingLots);
+        this.parkingLots = parkingLots;
+    }
+
+    public Ticket park(Car car) {
+        List<ParkingLot> notFullParkingLots = parkingLots.stream()
+                .filter(parkingLot -> !parkingLot.isFull())
+                .collect(Collectors.toList());
+        ParkingLot morePositionParkingLot = notFullParkingLots.stream()
+                .min(Comparator.comparingInt(parkingLot -> parkingLot.getParkedCar().size()))
+                .orElseThrow(noAnyPositionException::new);
+        return morePositionParkingLot.park(car);
+    }
+
+    public Car fetch(Ticket ticket) {
+        return new Car();
     }
 }
--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -24,4 +24,23 @@ public class SmartParkingBoyTest {
         assertEquals(1,firstParkingLot.getParkedCar().size());
         assertEquals(0,secondParkingLot.getParkedCar().size());
     }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_2_parking_lots_first_one_has_less_available_postiion_than_second_one() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(2);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car aliceCar = new Car();
+        smartParkingBoy.park(aliceCar);
+        Car bobCar = new Car();
+        //when
+        smartParkingBoy.park(bobCar);
+        //then
+        assertEquals(1,firstParkingLot.getParkedCar().size());
+        assertEquals(1,secondParkingLot.getParkedCar().size());
+    }
 }
